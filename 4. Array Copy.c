14c0e9280a7137988e183a6c82038ccb030b57a7
MD. Tanzim Rahman Udoy <udayrahman638650@gmail.com>
2026-01-01 18:45:48 +0600

    Add files via upload

---
+++ 4. Array Copy.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int arr1[100],arr2[100];
+    int i,n;
+    printf("\nCopy the elements of one array into another array");
+    printf("\n-------------------------------------------------\n");
+    printf("input the number of elements to be stored in the array");
+    scanf("%d",&n);
+    printf("input %d elements in the array:\n",n);
+    for(i=0;i<n;i++)
+    {
+        printf("elements-%d:",i);
+        scanf("%d",&arr1[i]);
+
+    }
+    for(i=0;i<n;i++)
+    {
+        arr2[i]=arr1[i];
+
+    }
+
+    printf("\nThe elements stored in the first array are :\n");
+    for(i=0;i<n;i++)
+    {
+        printf("% 5d", arr1[i]);
+
+    }
+     printf("\n\nThe elements copied into the second array are :\n");
+     for(i=0;i<n;i++)
+     {
+         printf("% 5d",arr2[i]);
+
+     }
+     printf("\n\n");
+
+
+}

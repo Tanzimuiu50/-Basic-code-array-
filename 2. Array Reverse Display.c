f13b6bbab78e3bc5de789803952eb2cbe5bcb8c3
MD. Tanzim Rahman Udoy <udayrahman638650@gmail.com>
2026-01-01 18:26:59 +0600

    Add files via upload

---
+++ 2. Array Reverse Display.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i;
+    printf("Enter the number of elements:\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+    printf("reveres array:\n");
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d",a[i]);
+    }
+}

fbd4614cce1176f90246917f2b6ce206ad73f9b7
MD. Tanzim Rahman Udoy <udayrahman638650@gmail.com>
2026-01-01 18:26:17 +0600

    Add files via upload

---
+++ Array Store & Print.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+   int arr[10];
+   int i;
+   printf("Read and print elements of an array\n");
+   printf("-----------------------------------\n");
+   printf("Input 10 elements in the array\n");
+
+   for( i=0;i<10;i++)
+   {
+       printf("element - %d :",i);
+       scanf("%d",&arr[i]);
+
+   }
+   printf("\nElements in array are:\n");
+
+    for(i=0;i<10;i++)
+    {
+        printf("%d",arr[i]);
+
+    }
+    printf("\n");
+    return 0;
+}
